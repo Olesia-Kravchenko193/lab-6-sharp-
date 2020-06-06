e97b42a600d00d2bdec9e0050c8be8f1a8a7517d
lesti <yaseloklass@gmail.com>
2020-06-07 01:03:35 +0300

    Добавьте файлы проекта.

---
+++ lab№6(sharp)/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace lab_6_sharp_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.GreenBox = new System.Windows.Forms.PictureBox();
+            this.GoldBox = new System.Windows.Forms.PictureBox();
+            this.BlueBox = new System.Windows.Forms.PictureBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.StopButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GoldBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(613, 363);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(135, 42);
+            this.ExitButton.TabIndex = 0;
+            this.ExitButton.Text = "Выход";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // GreenBox
+            // 
+            this.GreenBox.BackColor = System.Drawing.Color.Lime;
+            this.GreenBox.Location = new System.Drawing.Point(0, 200);
+            this.GreenBox.Name = "GreenBox";
+            this.GreenBox.Size = new System.Drawing.Size(42, 36);
+            this.GreenBox.TabIndex = 1;
+            this.GreenBox.TabStop = false;
+            // 
+            // GoldBox
+            // 
+            this.GoldBox.BackColor = System.Drawing.Color.Gold;
+            this.GoldBox.Location = new System.Drawing.Point(0, 23);
+            this.GoldBox.Name = "GoldBox";
+            this.GoldBox.Size = new System.Drawing.Size(42, 36);
+            this.GoldBox.TabIndex = 2;
+            this.GoldBox.TabStop = false;
+            // 
+            // BlueBox
+            // 
+            this.BlueBox.BackColor = System.Drawing.Color.Blue;
+            this.BlueBox.Location = new System.Drawing.Point(0, 109);
+            this.BlueBox.Name = "BlueBox";
+            this.BlueBox.Size = new System.Drawing.Size(42, 36);
+            this.BlueBox.TabIndex = 3;
+            this.BlueBox.TabStop = false;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(32, 363);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(102, 42);
+            this.StartButton.TabIndex = 4;
+            this.StartButton.Text = "Старт";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(140, 363);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(102, 42);
+            this.StopButton.TabIndex = 5;
+            this.StopButton.Text = "Стоп";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 170);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 10);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 75);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(776, 10);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MistyRose;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.GoldBox);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.GreenBox);
+            this.panel1.Controls.Add(this.BlueBox);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 261);
+            this.panel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.ExitButton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.GreenBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GoldBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.PictureBox GreenBox;
+        private System.Windows.Forms.PictureBox GoldBox;
+        private System.Windows.Forms.PictureBox BlueBox;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ lab№6(sharp)/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace lab_6_sharp_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Thread t1 = new Thread(Run1);
+            t1.Start();
+            t1.Join(100);           
+            Thread t2 = new Thread(Run2);
+            t2.Start();
+            t2.Join(100);
+            Thread t3 = new Thread(Run2);
+            t3.Start();
+            t3.Join(100);
+        }
+
+        Random r = new Random();
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Run1();
+            Run2();
+            Run3();
+        }
+        public void Run1()
+        {
+          GreenBox.Left += r.Next(10,30);
+          if (GreenBox.Left > 500)
+              timer1.Stop();
+        }
+        public void Run2()
+        {
+            BlueBox.Left += r.Next(10, 30);
+            if (BlueBox.Left > 500)
+                timer1.Stop();
+        }
+        public void Run3()
+        {
+            GoldBox.Left += r.Next(10, 30);
+            if (GoldBox.Left > 500)
+                timer1.Stop();
+        }
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+        private void Dropping(object sender, EventArgs e)
+        {
+            GoldBox.Location = new Point(0, 50);
+            BlueBox.Location = new Point(0, 50);
+            GreenBox.Location = new Point(0, 50);
+            timer1.Stop();
+        }
+
+    }
+}
